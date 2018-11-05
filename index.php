894715036a5b1ef27888ea75c33fc92789eb5bb9
David Grudl <david@grudl.com>
2018-11-05 11:57:38 +0100

    updates

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ use Nette\Application\Routers\Route;
 
 // Load libraries
 require __DIR__ . '/app/libs/nette.phar';
-require __DIR__ . '/app/libs/feed.class.php';
-require __DIR__ . '/app/libs/twitter.class.php';
+require __DIR__ . '/app/libs/Feed.php';
+require __DIR__ . '/app/libs/OAuth.php';
+require __DIR__ . '/app/libs/Twitter.php';
 require __DIR__ . '/app/libs/texy.phar';
 
 
@@ -25,11 +26,11 @@ $container = $configurator->createContainer();
 
 
 // Setup routes
-// http://davidgrudl.com/[cs|en]
+// https://davidgrudl.com/[cs|en]
 $router = $container->getService('router');
 $router[] = new Route('[<lang (?-i)cs|en>]', function ($presenter, $lang) use ($container) {
 	if (!$lang) {
-		$lang = $container->getService('httpRequest')->detectLanguage(array('en', 'cs')) ?: 'cs';
+		$lang = $container->getService('httpRequest')->detectLanguage(['en', 'cs']) ?: 'cs';
 		return $presenter->redirectUrl($lang);
 	}
 
@@ -42,7 +43,7 @@ $router[] = new Route('[<lang (?-i)cs|en>]', function ($presenter, $lang) use ($
 		if ($lang === 'en') {
 			return date('F j, Y', (int) $date);
 		} else {
-			static $months = array(1 => 'leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec');
+			static $months = [1 => 'leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec'];
 			$date = getdate((int) $date);
 			return "$date[mday]. {$months[$date['mon']]} $date[year]";
 		}
@@ -52,23 +53,38 @@ $router[] = new Route('[<lang (?-i)cs|en>]', function ($presenter, $lang) use ($
 		return Twitter::clickable($s);
 	});
 
-	$template->rss = ['Feed', 'loadRss'];
+	$template->twitter = function ($name) use ($container) {
+		try {
+			return $container->getService($name)->load(Twitter::ME);
+		} catch (TwitterException $e) {
+			return [];
+		}
+	};
+
+	$template->rss = function ($path) {
+		try {
+			return Feed::loadRss($path)->item;
+		} catch (FeedException $e) {
+			return [];
+		}
+	};
 
 	$template->addFilter('texy', function (Latte\Runtime\FilterInfo $info, $s) {
 		return (new Texy)->process($s);
 	});
+
 	return $template;
 });
 
 
-// http://davidgrudl.com/sources
-$router[] = new Route('sources', function ($presenter) {
+// https://davidgrudl.com/sources
+$router[] = new Route('sources', function ($presenter) use ($container) {
 
 	$template = $presenter->createTemplate()
 		->setFile(__DIR__ . '/app/sources.latte');
 
-	$template->source = function ($file, $lang = NULL) {
-		return preg_replace('#<br ?/?>#', '', highlight_file($file, TRUE));
+	$template->source = function ($file, $lang = null) {
+		return new Latte\Runtime\Html(preg_replace('#<br ?/?>#', '', highlight_file($file, true)));
 	};
 	return $template;
 });
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(function() {
-	$('#menu a[href^=#]').click(function() {
+	$('#menu a[href^="#"]').click(function() {
 		var hash = $(this).attr('href');
 		$('html,body').animate({ scrollTop: $(hash).offset().top - 5 }, 'fast');
 		return false;
 	});
 
-	$('a[href^=#more]').click(function() {
+	$('a[href^="#more"]').click(function() {
 		$('#more').slideDown();
 		return false;
 	});
